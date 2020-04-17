73997b518b32813a77904225d62d1f30c60c613e
Ingo Schmid <ingo@bananenbiegerei.de>
2020-04-17 17:52:45 +0200

    Newsletter signup form

---
+++ template-parts/partials/newsletter-signup-form.php
@@ -0,0 +1,42 @@
+<form action="https://t67a421c2.emailsys2a.net/190/4793/d40d75cfb1/subscribe/form.html" method="post" data-abide novalidate>
+  <div class="grid-x grid-margin-x margin-bottom-1">
+    <div class="cell small-12">
+      <label class="field_label required" for="email">E-Mail:
+        <input type="text" class="form_field" placeholder="max@muster.de" name="email" id="email" value="" aria-describedby="NewsletterEmail" aria-errormessage="emailError" required pattern="email" />
+        <span class="form-error">
+          Bitte geben Sie eine gültige E-Mail Adresse ein.
+        </span>
+      </label>
+      <label id="firstname_label" class="field_label" for="firstname">Name:
+        <input type="text" class="form_field" name="firstname" id="firstname" value="" />
+      </label>
+      <button class="button no-space" type="submit" value="Submit">Anmleden</button>
+    </div>
+  </div>
+</form>
+<a class="small" href="<?php the_permalink('3'); ?>">Hinweis zum Datenschutz</a>
+<?php /* <form action="https://t67a421c2.emailsys2a.net/190/4793/d40d75cfb1/subscribe/form.html" method="post">
+    <div class="form">
+        <div class="form_border">
+            <ul>
+                <li style="position:absolute; z-index: -100; left:-6000px;" aria-hidden="true">
+                    <label class="field_label required" for="rm_email">E-Mail: </label>
+                    <input type="text" class="form_field" name="rm_email" id="rm_email" value="" tabindex="-1" />
+                    <label class="field_label required" for="rm_comment">Comment: </label>
+                    <textarea class="form_field" name="rm_comment" tabindex="-1" id="rm_comment"></textarea>
+                </li>
+                <li>
+                    <label class="field_label required" for="email">E-Mail: </label>
+                    <input type="text" class="form_field" name="email" id="email" value="" />
+                </li>
+                <li id="firstname_form">
+                    <label id="firstname_label" class="field_label" for="firstname">Name: </label>
+                    <input type="text" class="form_field" name="firstname" id="firstname" value="" />
+                </li>
+                <li class="form_button">
+                    <input type="submit" class="form_button_submit" value="Anmelden" />
+                </li>
+            </ul>
+        </div>
+    </div>
+</form> */ ?>
