f640b1f5c87c1180170857be7d4edbbf487b9018
chr-ernst <ernst@edu-sharing.net>
2020-04-18 17:17:08 +0200

    smalle fixes;

--- template-parts/edusource/attributes.php
+++ template-parts/edusource/attributes.php
@@ -5,32 +5,41 @@
       $licences = get_field( 'licence' );
       if( $licences ):
         foreach ($licences as $licence):?>
-        <span class="multi-select"><?php echo $licence[label]; ?></span>
+        <span class="multi-select"><?php echo $licence['label']; ?></span>
       <?php endforeach;
       ?>
     <?php endif; ?>
   </li>
 <?php endif; ?>
+<?php if( get_field( 'costs' ) ) : ?>
+    <li>
+        <?php
+        $costs = get_field( 'costs' );
+        ?>
+        <?php echo esc_html($costs['label']); ?>
+    </li>
+<?php endif; ?>
+
+
+<?php if( get_field( 'advertisment' ) ) {
+    echo '<li>mit Werbung</li>';
+}else{
+    echo '<li>werbefrei</li>';
+} ?>
+
 <?php if( get_field( 'nutzung' ) ) : ?>
   <li>
     <?php
     $nutzungs = get_field( 'nutzung' );
     if( $nutzungs ):
       foreach ($nutzungs as $nutzung):?>
-      <span class="multi-select"><?php echo $nutzung[label]; ?></span>
+      <span class="multi-select"><?php echo $nutzung['label']; ?></span>
     <?php endforeach;
     ?>
   <?php endif; ?>
 </li>
 <?php endif; ?>
-<?php if( get_field( 'costs' ) ) : ?>
-<li>
-  <?php
-  $costs = get_field( 'costs' );
-  ?>
-  <?php echo esc_html($costs['label']); ?>
-</li>
-<?php endif; ?>
+
 <?php if( get_field( 'dsgvo' ) ) : ?>
 <li>
   <?php
@@ -47,17 +56,17 @@
   <?php echo esc_html($accessibility['label']); ?>
 </li>
 <?php endif; ?>
+<?php if( get_field( 'lang' ) ) : ?>
+    <li>
+        Sprache: <?php
+        $lang = get_field( 'lang' );
+        ?>
+        <?php echo esc_html($lang['label']); ?>
+    </li>
+<?php endif; ?>
 <?php if( get_field( 'anbieter' ) ) : ?>
 <li>
   Anbieter: <?php the_field('anbieter') ?>
-</li>
-<?php endif; ?>
-<?php if( get_field( 'lang' ) ) : ?>
-<li>
-  Sprache: <?php
-  $lang = get_field( 'lang' );
-  ?>
-  <?php echo esc_html($lang['label']); ?>
 </li>
 <?php endif; ?>
 </ul>
