3f925c708a41fc2fac1a8eed0d169e4635502d52
chr-ernst <ernst@edu-sharing.net>
2020-04-18 11:14:53 +0200

    update newsletter popup

--- template-parts/partials/newsletter-signup-form.php
+++ template-parts/partials/newsletter-signup-form.php
@@ -1,20 +1,21 @@
 <form action="https://t67a421c2.emailsys2a.net/190/4793/d40d75cfb1/subscribe/form.html" method="post" data-abide novalidate>
-  <div class="grid-x grid-margin-x margin-bottom-1">
-    <div class="cell small-12">
-      <label class="field_label required" for="email">E-Mail:
-        <input type="text" class="form_field" placeholder="max@muster.de" name="email" id="email" value="" aria-describedby="NewsletterEmail" aria-errormessage="emailError" required pattern="email" />
-        <span class="form-error">
-          Bitte geben Sie eine gültige E-Mail Adresse ein.
+    <div class="grid-x grid-margin-x margin-bottom-1">
+        <div class="cell small-12">
+            <h2>Anmeldung Newsletter:</h2>
+            <label class="field_label required" for="email">Deine E-Mail:
+                <input type="text" class="form_field" placeholder="max@muster.de" name="email" id="email" value="" aria-describedby="NewsletterEmail" aria-errormessage="emailError" required pattern="email" />
+                <span class="form-error">
+          Bitte geben Sie eine gÃ¼ltige E-Mail Adresse ein.
         </span>
-      </label>
-      <label id="firstname_label" class="field_label" for="firstname">Name:
-        <input type="text" class="form_field" name="firstname" id="firstname" value="" />
-      </label>
-      <button class="button no-space" type="submit" value="Submit">Anmleden</button>
+            </label>
+            <label id="firstname_label" class="field_label" for="firstname">Dein Name (freiwillig):
+                <input type="text" class="form_field" name="firstname" id="firstname" value="" />
+            </label>
+            <button class="button no-space" type="submit" value="Submit">Anmleden</button>
+        </div>
     </div>
-  </div>
 </form>
-<a class="small" href="<?php the_permalink('3'); ?>">Hinweis zum Datenschutz</a>
+<a class="small" href="<?php the_permalink('2719'); ?>">Hinweis zum Datenschutz</a>
 <?php /* <form action="https://t67a421c2.emailsys2a.net/190/4793/d40d75cfb1/subscribe/form.html" method="post">
     <div class="form">
         <div class="form_border">
