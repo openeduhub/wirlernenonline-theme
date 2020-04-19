2bf63b12154119f508aafb9b8802e623b3451257
Lucas Garbe <hi@lucasgarbe.de>
2020-04-19 15:28:48 +0200

    use float for checkboxes

--- template-parts/blocks/home-hero.php
+++ template-parts/blocks/home-hero.php
@@ -14,16 +14,16 @@
                 <input type="submit" class="button success" value="Suche">
               </div>
             </div>
-            <fieldset class="grid-x">
-              <div class="cell-shrink home-hero__checkbox">
+            <fieldset>
+              <div class="home-hero__checkbox">
                 <input id="material" type="checkbox" name="sourcetype" value="004"><label for="material">Lehrmaterial</label>
                 <span class="home-hero__checkbox__alias"></span>
               </div>
-              <div class="cell-shrink home-hero__checkbox">
+              <div class="home-hero__checkbox">
                 <input id="methods" type="checkbox" name="sourcetype" value="005,007"><label for="methods" name="fields">Lern-Tools</label>
                 <span class="home-hero__checkbox__alias"></span>
               </div>
-              <div class="cell-shrink home-hero__checkbox">
+              <div class="home-hero__checkbox">
                 <input id="databases" type="checkbox" name="sourcetype" value="001,002,003"><label for="databases">Medien</label>
                 <span class="home-hero__checkbox__alias"></span>
               </div>
