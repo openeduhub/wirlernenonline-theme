59d234cf16fc7f5775765091f2592df395f22369
Lucas Garbe <hi@lucasgarbe.de>
2020-04-19 17:10:49 +0200

    navigation with dropdown

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <?php get_template_part('head') ?>
 <body <?php body_class(); ?>>
 	<header id="site-header" class="site-header" role="banner">
-		<div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
+		<div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="large">
 			<button class="menu-icon" type="button" data-toggle></button>
 			<a class="mobile-logo-container" href="<?php echo get_home_url(); ?>">
 				<img class="logo" src="<?php echo get_stylesheet_directory_uri() ?>/dist/assets/img/WLO-Logo-01.png" alt="Logo">
@@ -13,15 +13,15 @@
 			<div class="top-bar-left position-relative">
 				<div class="grid-container">
 					<div class="grid-x grid-margin-x align-middle">
-						<div class="auto cell show-for-medium">
+						<div class="auto cell show-for-large">
 							<a href="<?php echo get_home_url(); ?>">
 								<img class="logo" src="<?php echo get_stylesheet_directory_uri() ?>/dist/assets/img/WLO-Logo-01.png" alt="Logo">
 							</a>
 						</div>
-						<div class="medium-shrink small-12 cell">
-							<?php wp_nav_menu( array( 'theme_location' => 'top', 'container' => false, 'depth' => 0, 'items_wrap' => '<ul class="vertical medium-horizontal menu top-menu" data-responsive-menu="drilldown medium-dropdown">%3$s</ul>', 'walker' => new insertcart_walker( array( 'in_top_bar' => true, 'item_type' => 'li', 'menu_type' => 'main-menu' ) ), ) ); ?>
+						<div class="large-shrink small-12 cell">
+							<?php wp_nav_menu( array( 'theme_location' => 'top', 'container' => false, 'depth' => 0, 'items_wrap' => '<ul class="vertical large-horizontal menu top-menu" data-responsive-menu="drilldown large-dropdown">%3$s</ul>', 'walker' => new insertcart_walker( array( 'in_top_bar' => true, 'item_type' => 'li', 'menu_type' => 'main-menu' ) ), ) ); ?>
 						</div>
-						<div class="cell small-12 medium-auto">
+						<div class="cell small-12 large-auto">
 							<?php wp_nav_menu( array( 'theme_location' => 'top-call-top-action', 'container' => false, 'depth' => 0, 'items_wrap' => '<ul class="menu top-call-to-action-menu align-right">%3$s</ul>', 'walker' => new insertcart_walker( array( 'in_top_bar' => true, 'item_type' => 'li', 'menu_type' => 'main-menu' ) ), ) ); ?>
 						</div>
 					</div>
