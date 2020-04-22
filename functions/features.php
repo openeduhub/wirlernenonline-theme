76146c9a3fcc74797efa39d46daee5d2b3123d7a
chr-ernst <ernst@edu-sharing.net>
2020-04-22 12:08:47 +0200

    more accessibillity fixes;

--- functions/features.php
+++ functions/features.php
@@ -12,7 +12,7 @@ function custom_excerpt_length( $length ) {
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
 function new_excerpt_more( $more ) {
-	return ' … <a class="read-more" href="' . get_permalink( get_the_ID() ) . '">' . __( ' <i class="fa fa-long-arrow-right"></i>' ) . '</a>';
+	return ' … <a class="read-more" aria-label="weiter lesen" href="' . get_permalink( get_the_ID() ) . '">' . __( ' <i class="fa fa-long-arrow-right"></i>' ) . '</a>';
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
 
