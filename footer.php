333d7acec270e8c3e9f95197851e9c3e9f88ec36
chr-ernst <ernst@edu-sharing.net>
2020-06-18 16:19:18 +0200

    changed newsletter button;

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 				<?php wp_nav_menu( array( 'theme_location' => 'footer', 'container' => false, 'depth' => 0, 'items_wrap' => '<ul class="dropdown menu footer-menu" data-dropdown-menu>%3$s</ul>', 'walker' => new insertcart_walker( array( 'in_top_bar' => true, 'item_type' => 'li', 'menu_type' => 'main-menu' ) ), ) ); ?>
 			</div>
 			<div class="cell shrink">
-				<button class="button small no-space" data-open="newsletterModal">Newsletter Anmeldung</button>
+				<button class="button small no-space" data-open="newsletterModal">Newsletter</button>
 				<div class="reveal" id="newsletterModal" data-reveal>
 				  <?php get_template_part('template-parts/partials/newsletter-signup-form') ?>
 				</div>
