0b939734c4c2008b209874060253fd608d071933
chr-ernst <ernst@edu-sharing.net>
2020-06-18 16:21:01 +0200

    added slick,js for slider

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -3,6 +3,7 @@ function site_scripts() {
     //JS
     wp_enqueue_script( 'site-js', get_template_directory_uri() . '/dist/assets/js/app.js', array( 'jquery' ), '', true );
     wp_enqueue_script( 'sort-table', get_template_directory_uri() . '/src/assets/js/sort-table.min.js', array(), '', true );
+    wp_enqueue_script( 'slick', get_template_directory_uri() . '/dist/assets/js/slick.min.js', array( 'jquery' ), '', true );
 
     if (is_page_template('archive-edusource.php') || is_page_template('archive-edutool.php') || is_front_page() || true) {
         wp_enqueue_script( 'select2-js', get_template_directory_uri() . '/dist/assets/js/select2.min.js', array( 'jquery' ), '', true );
@@ -16,6 +17,8 @@ function site_scripts() {
 
     // CSS
     wp_enqueue_style( 'select2-css', get_template_directory_uri() . '/dist/assets/scss/select2.css', array(), '20200430', 'all' );
+    wp_enqueue_style( 'slick-css', get_template_directory_uri() . '/dist/assets/scss/slick.css', array(), '20200430', 'all' );
+    wp_enqueue_style( 'slick-theme-css', get_template_directory_uri() . '/dist/assets/scss/slick-theme.css', array(), '20200430', 'all' );
     wp_enqueue_style( 'site-css', get_template_directory_uri() . '/dist/assets/scss/app.css', array(), '20200430', 'all' );
     wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css', array(), '20200430', 'all' );
 }
