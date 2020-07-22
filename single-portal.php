43d3ce9258f471e73df41632c2504789978644c1
Maximilian Heller <heller.maximilian@gmail.com>
2020-07-22 13:45:39 +0200

    * TP: Changing Portal-Color/BG-Color now having an effect of presentation

--- single-portal.php
+++ single-portal.php
@@ -3,6 +3,15 @@ get_header();
 ?>
 <?php
 while (have_posts()) : the_post(); ?>
+    <style type="text/css">
+        .portal {
+            background: <?php the_field('background_color'); ?>;
+        }
+
+        .portal h1, .portal h2,.portal h3,.portal h4,.portal h5,.portal h6, .portal a{
+            color: <?php the_field('font_color'); ?>;
+        }
+    </style>
     <div class="portal">
         <div class="grid-container">
             <div class="grid-x grid-margin-x">
