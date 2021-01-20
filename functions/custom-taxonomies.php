ee4e209def2b644935adcf310f7e18c659c0b785
Torsten Simon <simon@edu-sharing.net>
2021-01-20 17:57:58 +0100

    fix:custom taxonomies

--- functions/custom-taxonomies.php
+++ functions/custom-taxonomies.php
@@ -1,33 +1,30 @@
 <?php
 
-function recurseBuildTaxonomy($taxonomyId, $skos, int $parent){
+function recurseBuildTaxonomy($taxonomy, $skos, int $parent){
+    $taxonomyId = $taxonomy->name;
     foreach($skos as $s){
         $idStr = $s->id;
         $lastSlash = strrpos($idStr, "/");
         $id = substr($idStr, $lastSlash + 1);
-        //$field['choices'][ $id ] = $choice->prefLabel->de;
-        $parent = wp_insert_term($s->prefLabel->de, $taxonomyId, [ 'slug' => $id, 'parent' => $parent])['term_id'];
-        if($s -> narrower){
-            recurseBuildTaxonomy($taxonomyId, $s -> narrower, $parent);
+        $term = get_term_by('slug', $id, $taxonomyId);
+        if($term){
+            $parent2 = $term->term_id;
+            wp_update_term($term->term_id, $taxonomyId, [
+                'parent' => $parent,
+                'slug' => $id,
+                'name' => $s->prefLabel->de
+            ]);
+        } else {
+            $parent2 = wp_insert_term($s->prefLabel->de, $taxonomyId, [ 'slug' => $id ])['term_id'];
         }
+        if($s -> narrower){
+            recurseBuildTaxonomy($taxonomy, $s -> narrower, $parent2);
+        }
+
     }
 }
 function buildTaxonomyForField($field, $taxonomyName, $taxonomyId, $vocabUrl){
-
-    $transient = 'vocab_'.$taxonomyId;
-    $vocab_json = null;
-    if (false === ( $value = get_transient( $transient ) ) ) {
-        // this code runs when there is no valid transient set
-        // Get Select-Field Options from Vocab Scheme
-        $json = file_get_contents($vocabUrl);
-        $vocab_json = json_decode($json);
-        set_transient( $transient, $vocab_json, 60*60*12 );
-    } else{
-        $field['taxonomy'] = $taxonomyId;
-        return;
-    }
-
-    register_taxonomy($taxonomyId, 'method', array(
+    $taxonomy=register_taxonomy($taxonomyId, 'method', array(
         'hierarchical' => true,
         'labels' => array(
             'name' => _x( $taxonomyName, 'taxonomy general name' ),
@@ -37,9 +34,21 @@ function buildTaxonomyForField($field, $taxonomyName, $taxonomyId, $vocabUrl){
         'show_admin_column' => false,
         'query_var' => true,
     ));
-
-    // cleanup terms
-
+    $transient = 'vocab_'.$taxonomyId;
+    $vocab_json = null;
+    if (false === ( $value = get_transient( $transient ) ) ) {
+        // this code runs when there is no valid transient set
+        // Get Select-Field Options from Vocab Scheme
+        $json = file_get_contents($vocabUrl);
+        $vocab_json = json_decode($json);
+        set_transient( $transient, $vocab_json, 60*60*12 );
+    } else{
+        //$vocab_json = get_transient( $transient );
+        $field['taxonomy'] = $taxonomyId;
+        return $field;
+    }
+    // cleanup terms: NO! Will invalid all previously set values!!!
+    /*
     $terms = get_terms( array(
         'taxonomy' => $taxonomyId,
         'hide_empty' => false,
@@ -47,20 +56,11 @@ function buildTaxonomyForField($field, $taxonomyName, $taxonomyId, $vocabUrl){
     foreach ( $terms as $value ) {
         wp_delete_term( $value->term_id, $taxonomyId);
     }
+    */
 
     $choices = $vocab_json->hasTopConcept;
     if( is_array($choices) ) {
-        foreach( $choices as $choice ) {
-
-            $idStr = $choice->id;
-            $lastSlash = strrpos($idStr, "/");
-            $id = substr($idStr, $lastSlash + 1);
-            //$field['choices'][ $id ] = $choice->prefLabel->de;
-            $parent = wp_insert_term($choice->prefLabel->de, $taxonomyId, [ 'slug' => $id ])['term_id'];
-            if($choice -> narrower){
-                recurseBuildTaxonomy($taxonomyId, $choice -> narrower, $parent);
-            }
-        }
+        recurseBuildTaxonomy($taxonomy, $choices, 0);
     }
     $field['taxonomy'] = $taxonomyId;
     return $field;
