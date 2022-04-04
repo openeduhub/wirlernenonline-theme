d47705f0e7d6ed7aa1de202a8fea9edd37e1abff
chr-ernst <ernst@edu-sharing.net>
2022-04-04 12:14:41 +0200

    edit wlo-redaktion-btn;

--- template_login.php
+++ template_login.php
@@ -13,8 +13,8 @@ get_header();?>
 
 
     if (is_user_logged_in()) { ?>
-        <div class="wp-block-buttons aligncenter">
-            <div class="wp-block-button"><a class="wp-block-button__link has-text-color has-very-light-gray-color has-background has-very-dark-gray-background-color" href="https://wirlernenonline.de/redaktionsumgebung-organisatorisches/">Zur Redaktionsumgebung</a></div>
+        <div class="wp-block-buttons aligncenter wlo-redaktion-btn">
+            <div class="wp-block-button"><a class="wp-block-button__link has-text-color has-very-light-gray-color has-background has-very-dark-gray-background-color" href="https://wirlernenonline.de/intranet-aktuelles-lernen/">Zur Redaktionsumgebung</a></div>
         </div>
     <?php };
     ?>
