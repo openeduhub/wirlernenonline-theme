abef46ef7262ce23d78f02acfa00f9854e3896b0
Christian Ernst <ernst@edu-sharing.net>
2022-06-27 09:35:09 +0200

    remove unused css;

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -5,20 +5,14 @@
 function site_scripts() {
     $versionDate = '20200821';
     //JS
-    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/dist/assets/js/app.js', array( 'jquery' ), '', true );
+    //wp_enqueue_script( 'site-js', get_template_directory_uri() . '/dist/assets/js/app.js', array( 'jquery' ), '', true );
     wp_enqueue_script( 'slick', get_template_directory_uri() . '/dist/assets/js/slick.min.js', array( 'jquery' ), '', true );
 
-    if (is_page_template('archive-edusource.php') || is_page_template('archive-edutool.php') || is_front_page() || true) {
-        wp_enqueue_script( 'select2-js', get_template_directory_uri() . '/dist/assets/js/select2.min.js', array( 'jquery' ), '', true );
-        wp_enqueue_script( 'isotope-js', get_template_directory_uri() . '/dist/assets/js/isotope.min.js', array( 'jquery' ), '', true );
-        wp_enqueue_script( 'imagesloaded-js', get_template_directory_uri() . '/dist/assets/js/imagesloaded.min.js', array( 'jquery' ), '', true );
-        wp_enqueue_script( 'lazyload-js', get_template_directory_uri() . '/dist/assets/js/lazyload.min.js', array( 'jquery' ), '', true );
-        wp_enqueue_script( 'lernstoff-js', get_template_directory_uri() . '/dist/assets/js/lernstoff.js', array( 'jquery', 'isotope-js', 'imagesloaded-js', 'select2-js', 'lazyload-js' ), '', true );
-    }
+
 
     if ( is_singular( 'portal' ) ) {
-        wp_enqueue_script( 'chart-js', get_template_directory_uri() . '/dist/assets/js/Chart.min.js', array( 'jquery' ), '', false );
-        wp_enqueue_style( 'chart-css', get_template_directory_uri() . '/dist/assets/scss/Chart.css', array(), $versionDate, 'all' );
+        //wp_enqueue_script( 'chart-js', get_template_directory_uri() . '/dist/assets/js/Chart.min.js', array( 'jquery' ), '', false );
+        //wp_enqueue_style( 'chart-css', get_template_directory_uri() . '/dist/assets/scss/Chart.css', array(), $versionDate, 'all' );
     }
 
     // source_table
