a1348481ed2c820e7393b93b77774b428474100b
Christian Ernst <ernst@edu-sharing.net>
2022-07-12 13:19:42 +0200

    optional mainteneance poup;

--- front-page.php
+++ front-page.php
@@ -54,6 +54,20 @@ foreach ($subjects_vocab as $subject){
 }
 
 ?>
+
+<?php
+if ( get_field('maintenance') ) { ?>
+    <div class="maintenance-popup">
+        <button class="maintenance-popup-close" onclick="jQuery('.maintenance-popup').hide('fast')">X</button>
+        <img src="<?php echo get_field('maintenance_img'); ?>">
+        <div class="maintenance-text">
+            <h2><?php echo get_field('maintenance_header'); ?></h2>
+            <p><?php echo get_field('maintenance_text'); ?></p>
+        </div>
+    </div>
+<?php }
+?>
+
     <div class="wlo-page">
 
         <div class="wlo-header wlo-frontpage-header">
