3039cafffbffdd21d6504f8c29284d78788c286b
Christian Ernst <ernst@edu-sharing.net>
2022-08-09 16:05:17 +0200

    only allow loggedin users;

--- header-editorial-desk.php
+++ header-editorial-desk.php
@@ -3,6 +3,12 @@
 <?php get_template_part('head') ?>
 
 <?php
+
+if(!is_user_logged_in()) {
+    wp_redirect( wp_login_url() );
+    exit();
+}
+
 $user = wp_get_current_user();
 
 if (function_exists('get_repo_ticket')){
