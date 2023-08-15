fc6f4491db755cc210d8023151744d29d5d3737a
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-08-15 15:20:46 +0200

    fix: links on login page

--- template_login.php
+++ template_login.php
@@ -1,5 +1,5 @@
 <?php /* Template Name: Login */
-get_header();?>
+get_header(); ?>
 <div class="gutenberg-layout-container">
     <div class="grid-x grid-margin-x">
         <div class="medium-12 cell">
@@ -11,17 +11,25 @@ get_header();?>
         the_content();
     endwhile;
 
-    if (function_exists('get_repo_ticket')){
+    if (function_exists('get_repo_ticket')) {
         $ticket = get_repo_ticket();
-    }else{
+    } else {
         $ticket = '';
     }
 
 
     if (is_user_logged_in()) { ?>
         <div class="wp-block-buttons aligncenter wlo-redaktion-btn">
-            <div class="wp-block-button"><a class="wp-block-button__link has-text-color has-very-light-gray-color has-background has-very-dark-gray-background-color" href="https://wirlernenonline.de/intranet-aktuelles-lernen/">Zur Redaktionsumgebung</a></div>
-            <div class="wp-block-button"><a class="wp-block-button__link has-text-color has-very-light-gray-color has-background has-very-dark-gray-background-color" href="https://redaktion.openeduhub.net/edu-sharing/components/collections?ticket=<?php echo $ticket; ?>">Zum edu-sharing</a></div>
+            <div class="wp-block-button">
+                <a
+                    class="wp-block-button__link has-text-color has-very-light-gray-color has-background has-very-dark-gray-background-color"
+                    href="/intranet-aktuelles-lernen/">Zur Redaktionsumgebung</a>
+            </div>
+            <div class="wp-block-button">
+                <a
+                    class="wp-block-button__link has-text-color has-very-light-gray-color has-background has-very-dark-gray-background-color"
+                    href="<?php echo WLO_REPO; ?>components/collections?ticket=<?php echo $ticket; ?>">Zum edu-sharing</a>
+            </div>
         </div>
     <?php };
     ?>
