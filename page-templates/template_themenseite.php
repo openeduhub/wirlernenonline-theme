47c2367cfe016fbea07ece48fabe28e5c7e2b875
steinhaus <steinhaus@edu-sharing.net>
2024-02-28 19:01:21 +0100

    fix: removed empty accordion in cases where no content is to display

--- page-templates/template_themenseite.php
+++ page-templates/template_themenseite.php
@@ -572,6 +572,8 @@ while (have_posts()) : the_post(); ?>
                     <div class="fachportal-header-accordion">
 
                         <div class="wlo-accordion-wrapper" style="background-color:rgba(<?php echo $rgbBackgroundColor; ?>, 0.2);">
+                            <?php
+                            if (!empty($contentArray)) { ?>
                             <button class="wlo-accordion" id="fachportal-accordion-<?php echo $accordionID; ?>">
                                 <h2>Die neusten geprüften Inhalte für dich!</h2>
                                 <img class="wlo-accordion-icon" src="<?php echo get_template_directory_uri(); ?>/src/assets/img/arrow_down.svg" alt="Inhalte ein odder ausklappen">
@@ -580,7 +582,6 @@ while (have_posts()) : the_post(); ?>
                             <div class="wlo-accordion-content" id="<?php echo $sliderId; ?>">
 
                                 <?php
-                                if (!empty($contentArray)) {
                                     foreach (array_slice($contentArray, 0, get_field('content_count')) as $content) { ?>
                                         <div class="widget-content<?php if (!empty($content['resourcetype'])) {
                                                                         foreach ($content['resourcetype'] as $type) {
@@ -588,78 +589,78 @@ while (have_posts()) : the_post(); ?>
                                                                         }
                                                                     } ?>">
 
-                                            <button onclick="showContentPopup('<?php echo $content['id']; ?>')">
+                                        <button onclick="showContentPopup('<?php echo $content['id']; ?>')">
 
-                                                <?php if (!empty($content['image_url'])) { ?>
-                                                    <img class="main-image" src="<?php echo $content['image_url']; ?>" alt="Cover: <?php echo $content['title']; ?>">
-                                                <?php } ?>
-                                                <div class="content-info">
-                                                    <div class="content-header">
-                                                        <?php if ($content['source']) { ?>
-                                                            <p class="content-source"><?php echo $content['source']; ?></p>
-                                                        <?php } ?>
-                                                        <img class="badge" src="<?php echo get_template_directory_uri(); ?>/src/assets/img/badge_green.svg" alt="Auszeichnung: geprüfter Inhalt">
-                                                        <?php if ($content['oer']) { ?>
-                                                            <div class="badge ">OER</div>
-                                                        <?php } ?>
-                                                    </div>
-                                                    <div class="content-title"><?php echo $content['title']; ?></div>
-                                                    <p class="content-description"><?php echo $content['description'] ?></p>
-                                                    <div class="content-meta">
-                                                        <?php if (!empty($content['resourcetype'])) {
-                                                            echo '<img src="' . get_template_directory_uri() . '/src/assets/img/img_icon.svg"  alt="Materialart">';
-                                                            echo '<p>';
-                                                            $i = 0;
-                                                            foreach ($content['resourcetype'] as $type) {
-                                                                if (++$i === count($content['resourcetype'])) {
-                                                                    echo $type;
-                                                                } else {
-                                                                    echo $type . ', ';
-                                                                }
+                                            <?php if (!empty($content['image_url'])) { ?>
+                                                <img class="main-image" src="<?php echo $content['image_url']; ?>" alt="Cover: <?php echo $content['title']; ?>">
+                                            <?php } ?>
+                                            <div class="content-info">
+                                                <div class="content-header">
+                                                    <?php if ($content['source']) { ?>
+                                                        <p class="content-source"><?php echo $content['source']; ?></p>
+                                                    <?php } ?>
+                                                    <img class="badge" src="<?php echo get_template_directory_uri(); ?>/src/assets/img/badge_green.svg" alt="Auszeichnung: geprüfter Inhalt">
+                                                    <?php if ($content['oer']) { ?>
+                                                        <div class="badge ">OER</div>
+                                                    <?php } ?>
+                                                </div>
+                                                <div class="content-title"><?php echo $content['title']; ?></div>
+                                                <p class="content-description"><?php echo $content['description'] ?></p>
+                                                <div class="content-meta">
+                                                    <?php if (!empty($content['resourcetype'])) {
+                                                        echo '<img src="' . get_template_directory_uri() . '/src/assets/img/img_icon.svg"  alt="Materialart">';
+                                                        echo '<p>';
+                                                        $i = 0;
+                                                        foreach ($content['resourcetype'] as $type) {
+                                                            if (++$i === count($content['resourcetype'])) {
+                                                                echo $type;
+                                                            } else {
+                                                                echo $type . ', ';
                                                             }
-                                                            echo '</p>';
-                                                        } ?>
-                                                    </div>
-                                                    <div class="content-meta">
-                                                        <?php if (!empty($content['subjects'])) {
-                                                            echo '<img src="' . get_template_directory_uri() . '/src/assets/img/subject_icon.svg"  alt="Fächer">';
-                                                            echo '<p>';
-                                                            $i = 0;
-                                                            foreach ($content['subjects'] as $subject) {
-                                                                if (++$i === count($content['subjects'])) {
-                                                                    echo $subject;
-                                                                } else {
-                                                                    echo $subject . ', ';
-                                                                }
+                                                        }
+                                                        echo '</p>';
+                                                    } ?>
+                                                </div>
+                                                <div class="content-meta">
+                                                    <?php if (!empty($content['subjects'])) {
+                                                        echo '<img src="' . get_template_directory_uri() . '/src/assets/img/subject_icon.svg"  alt="Fächer">';
+                                                        echo '<p>';
+                                                        $i = 0;
+                                                        foreach ($content['subjects'] as $subject) {
+                                                            if (++$i === count($content['subjects'])) {
+                                                                echo $subject;
+                                                            } else {
+                                                                echo $subject . ', ';
                                                             }
-                                                            echo '</p>';
-                                                        } ?>
-                                                    </div>
-                                                    <div class="content-meta">
-                                                        <?php if (!empty($content['educationalcontext'])) {
-                                                            echo '<img src="' . get_template_directory_uri() . '/src/assets/img/class_icon.svg"  alt="Bildungsebene">';
-                                                            echo '<p>';
-                                                            $i = 0;
-                                                            foreach ($content['educationalcontext'] as $subject) {
-                                                                if (++$i === count($content['educationalcontext'])) {
-                                                                    echo $subject;
-                                                                } else {
-                                                                    echo $subject . ', ';
-                                                                }
+                                                        }
+                                                        echo '</p>';
+                                                    } ?>
+                                                </div>
+                                                <div class="content-meta">
+                                                    <?php if (!empty($content['educationalcontext'])) {
+                                                        echo '<img src="' . get_template_directory_uri() . '/src/assets/img/class_icon.svg"  alt="Bildungsebene">';
+                                                        echo '<p>';
+                                                        $i = 0;
+                                                        foreach ($content['educationalcontext'] as $subject) {
+                                                            if (++$i === count($content['educationalcontext'])) {
+                                                                echo $subject;
+                                                            } else {
+                                                                echo $subject . ', ';
                                                             }
-                                                            echo '</p>';
-                                                        } ?>
-                                                    </div>
-
-                                                    <a class="content-button" href="<?php echo $content['content_url']; ?>" target="_blank" aria-label="Zum-Inhalt: <?php echo $content['title']; ?>">Zum Inhalt</a>
-
+                                                        }
+                                                        echo '</p>';
+                                                    } ?>
                                                 </div>
 
+                                                <a class="content-button" href="<?php echo $content['content_url']; ?>" target="_blank" aria-label="Zum-Inhalt: <?php echo $content['title']; ?>">Zum Inhalt</a>
 
-                                        </div>
-                                <?php }
-                                } ?>
+                                            </div>
+
+
+                                    </div>
+                                <?php } ?>
                             </div>
+                            <?php } ?>
 
                             <div class="wlo-accordion-bottom"></div>
 
