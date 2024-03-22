4b70cc2e136b11834fd215ea0150dd492740d921
steinhaus <steinhaus@edu-sharing.net>
2024-03-22 19:51:22 +0100

    fix: lodash/underscore conflict (this.activateMode is not a function in gutenberg editor)

--- functions/gutenberg-blocks.php
+++ functions/gutenberg-blocks.php
@@ -422,5 +422,6 @@ function register_edusharing_blocks()
         'icon'              => '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path fill="none" d="M0 0h24v24H0z"></path><path style="fill: #A2A2A2" d="M3 15h18v-2H3v2zm0 4h18v-2H3v2zm0-8h18V9H3v2zm0-6v2h18V5H3z"></path></svg>',
         'editor_script'     => 'portal-edusharing-list-script',
     ));
+    wp_enqueue_script('fix-lodash-conflicts', get_template_directory_uri() . '/src/assets/js/fixLodashConflict.js', array('jquery'), '', true);
 }
 add_action('init', 'register_edusharing_blocks');
