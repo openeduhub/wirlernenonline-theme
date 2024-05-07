58bc9639e8342250237dbd2e1dee39a1fa504249
steinhaus <steinhaus@edu-sharing.net>
2024-05-07 15:38:43 +0200

    feat: added user group mappings to the login redirection

--- functions/login-redirect.php
+++ functions/login-redirect.php
@@ -12,7 +12,38 @@ function wlo_login_redirect($user_login,$user) {
             } else {
                 $ticket = '';
             }
-            wp_redirect(WLO_REPO . "/components/start?ticket=" . $ticket);
+            // the first matching group the person is member of will map to the URL!
+            $MAPPINGS = [
+//                'GROUP_7ea16e92-eadc-103a-81f6-9d9aea029be1' => '80151374-f53a-49f2-8516-0a6ae41d7a05', // Schule im Aufbruch
+//                'GROUP_cf7c3cf8-41c5-103b-88aa-2b6a146a3545' => '627d249d-61e4-4860-b739-bb3c8b21946e', // rpi virtuell
+//                'GROUP_513e4a78-4a56-103a-84e4-2b017690ecd2' => 'f036ab86-7089-4130-8d4e-c89d5bb169f0', // WLO-Redaktion
+                'GROUP_ORG_Projektmanagement (GPM)' => 'https://redaktion.projektmanagement.wirlernenonline.de/edu-sharing/', // GPM
+                null => WLO_REPO, // others or unknown
+            ];
+            $user = wp_get_current_user();
+            $redirectURL = $MAPPINGS[null];
+
+            try {
+                $apiUrl = 'rest/iam/v1/people/-home-/-me-/memberships?maxItems=100&skipCount=0';
+                foreach($MAPPINGS as $groupName => $repoContext) {
+                    if($groupName == null) {
+                        continue;
+                    }
+                    foreach (callRepoApi($apiUrl, null, 'Content-Type: application/json', 'GET', $ticket)["groups"] as $group) {
+                        if($groupName == $group["authorityName"]) {
+                            $redirectURL = $repoContext;
+                            //error_log('user is in group ' . $group["profile"]["displayName"] . ' -> set home folder to ' . $folder);
+                            break 2;
+                        }
+                    }
+                }
+
+            } catch(Exception $e) {
+                error_log('could not fetch groups for user ' . $user->user_login . ', defaulting to unknown');
+            }
+            $redirectURL = $redirectURL . "components/start?ticket=" . $ticket;
+
+            wp_redirect($redirectURL);
             die();
         }
     }
